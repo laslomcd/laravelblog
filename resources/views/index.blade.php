fb97587f5ff0444491ba3b1d7194e1b79d9fc42e
ryan <laslomcd@gmail.com>
2021-05-26 11:08:56 -0400

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5">
+
+            </div>
+        </div>
+    </div>
+
+@endsection
